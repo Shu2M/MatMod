af3364e837ca8c66ff8c673e286768078353aa4a
Nathanoul <92688201+Nathanoul@users.noreply.github.com>
2022-05-30 12:13:14 +0300

    Update SolveModule.py

--- SolveModule.py
+++ SolveModule.py
@@ -1,13 +1,13 @@
-import wx
 import numpy as np
 from numpy import multiply as mul
 from numpy import divide as div
 
 
-class GrowTask(wx.Panel):
-    def __init__(self, Gamma_r_list, Gamma_t_list, Gamma_z_list, A_list, mu, N, P, dr_s=0.001, dr_m=0.001):
+class GrowTask():
+    def __init__(self, Gamma_r_text, Gamma_t_text, Gamma_z_text, A_list, mu, N, P, dr_s=0.01, dr_m=0.01):
 
         A = A_list
+        self.A = A
         self.dr_s = dr_s
         self.dr_m = dr_m
 
@@ -45,17 +45,29 @@ class GrowTask(wx.Panel):
             S[-1] += s_b
             return np.array(S)
 
-        def combine(Fun_list, A, dx):
-            Fun_comb = []
-            X_span = np.arange(A[0], A[-1] + dx, dx)
-            len_A = len(A)
+        def text2list(Text_list, A, dx):
+            X = [x for x in np.arange(A[0], A[-1] + dx/2, dx)]
+            Fun_list = np.zeros([len(Text_list), len(X)])
+            for i in range(0, len(Text_list)):
+                j = 0
+                for x in X:
+                    if type(Text_list[i]) == 'string':
+                        Fun_list[i, j] = eval(Text_list[i])
+                        j += 1
+                    else:
+                        Fun_list[i, j] = Text_list[i]
+                        j += 1
 
+            return Fun_list
+
+        def combine(Fun_list, A, dx):
+            Fun_comb = np.zeros(len(Fun_list[0, :]))
             shell_id = 0
-            for x in X_span:
-                if shell_id < len_A:
-                    if x > A[shell_id]:
+            for i in range(0, len(Fun_list[0, :])):
+                if shell_id < len(A) - 2:
+                    if A[0] + i*dx >= A[shell_id + 1]:
                         shell_id += 1
-                Fun_comb.append(eval(Fun_list[shell_id]))
+                Fun_comb[i] = Fun_list[shell_id, i]
 
             return Fun_comb
 
@@ -73,53 +85,78 @@ class GrowTask(wx.Panel):
                 i += 1
             return np.array(Fun_inv)
 
-        G_r = combine(Gamma_r_list, A, dr_s)
-        G_t = combine(Gamma_t_list, A, dr_s)
-        G_z = combine(Gamma_z_list, A, dr_s)
-        G_f = mul(mul(G_r, G_t), G_z)
+        Gamma_r_list = text2list(Gamma_r_text, A, dr_s)
+        Gamma_t_list = text2list(Gamma_t_text, A, dr_s)
+        Gamma_z_list = text2list(Gamma_z_text, A, dr_s)
+        Gam_r = combine(Gamma_r_list, A, dr_s)
+        Gam_t = combine(Gamma_t_list, A, dr_s)
+        Gam_z = combine(Gamma_z_list, A, dr_s)
+        G = mul(mul(Gam_r, Gam_t), Gam_z)
 
         a = A[0] ** 2
         zeta = 1
-        y = zeta * a
+        y = a*zeta
         y_old = y
+        lam = 10 ** -1
 
-        X = [x for x in np.arange(A[0], A[-1] + dr_s, dr_s)]
-        I_g = integrate(A[0], A[-1], mul(G_f, X), A[0], dr_s)
-        i_t = integrate(A[0], A[-1], div(mul(G_f, X),
-                                                   mul(G_t, G_t)), A[0], dr_s)[-1]
-        i_z = integrate(A[0], A[-1], div(mul(G_f, X),
-                                                   mul(G_z, G_z)), A[0], dr_s)[-1]
+        ###
+        X = [x for x in np.arange(A[0], A[-1] + dr_s/2, dr_s)]
+        I_g = integrate(A[0], A[-1], mul(G, X), A[0], dr_s)
+        i_t = integrate(A[0], A[-1], div(G,
+                                         mul(X, mul(Gam_t, Gam_t))), A[0], dr_s)[-1]
+        i_z = integrate(A[0], A[-1], div(mul(G, X),
+                                         mul(Gam_z, Gam_z)), A[0], dr_s)[-1]
 
-        I_y1_arg = div(mul(mul(mul(mul(X, X), X), mul(G_t, G_t)), mul(G_z, G_z)),
-                       mul((y + 2 * I_g), (y + 2 * I_g)))
-        I_y2_arg = div((y + 2 * I_g),
-                       mul(X, mul(G_t, G_t)))
+        I_y1_arg = div(mul(mul(mul(X, X), X), mul(mul(G, G), G)),
+                       mul(mul(y + 2 * I_g, y + 2 * I_g), mul(Gam_r, Gam_r)))
         i_y1 = integrate(A[0], A[-1], I_y1_arg, A[0], dr_s)[-1]
-        i_y2 = integrate(A[0], A[-1], I_y2_arg, A[0], dr_s)[-1]
-        zeta = mu / P * (i_t - i_y1)
-        y = 1 / P * (zeta / np.pi * N - mu * zeta ** 2 * i_z + mu / zeta * i_y2)
+        y = y + (1 - mu / P * (i_t - i_y1))
+        y = y + (1 - mu / P * (i_t - i_y1))
+        y = y + (1 - mu / P * (i_t - i_y1))
 
-        eps = abs(y_old - y)
+        eps = 1
         while eps >= 1e-6:
             y_old = y
-            I_y1_arg = div(mul(mul(mul(mul(X, X), X), mul(G_t, G_t)), mul(G_z, G_z)),
-                           mul((y + 2 * I_g), (y + 2 * I_g)))
-            I_y2_arg = div((y + 2 * I_g),
-                           mul(X, mul(G_t, G_t)))
+            I_y1_arg = div(mul(mul(mul(X, X), X), mul(mul(G, G), G)),
+                           mul(mul(y + 1 ** -3 + 2 * I_g, y + 1 ** -3 + 2 * I_g), mul(Gam_r, Gam_r)))
+            I_y2_arg = div(mul(mul(mul(X, X), X), mul(mul(G, G), G)),
+                           mul(y + 1 ** -3 + 2 * I_g, mul(Gam_r, Gam_r)))
+            I_y3_arg = div(mul(G, y + 1 ** -3 + 2 * I_g),
+                           mul(X, mul(Gam_t, Gam_t)))
             i_y1 = integrate(A[0], A[-1], I_y1_arg, A[0], dr_s)[-1]
             i_y2 = integrate(A[0], A[-1], I_y2_arg, A[0], dr_s)[-1]
+            i_y3 = integrate(A[0], A[-1], I_y3_arg, A[0], dr_s)[-1]
             zeta = mu / P * (i_t - i_y1)
-            y = 1 / P * (zeta / np.pi * N - mu * zeta ** 2 * i_z + mu / zeta * i_y2)
+            f_1 = mu / (N * P) * (2 * zeta ** 5 * i_z - i_y2 - i_y3 * zeta ** 2)\
+                  + y * zeta ** 3 / N - zeta ** 4 / (np.pi * P)
+
+            I_y1_arg = div(mul(mul(mul(X, X), X), mul(mul(G, G), G)),
+                           mul(mul(y + 2 * I_g, y + 2 * I_g), mul(Gam_r, Gam_r)))
+            I_y2_arg = div(mul(mul(mul(X, X), X), mul(mul(G, G), G)),
+                           mul(y + 2 * I_g, mul(Gam_r, Gam_r)))
+            I_y3_arg = div(mul(G, y + 2 * I_g),
+                           mul(X, mul(Gam_t, Gam_t)))
+            i_y1 = integrate(A[0], A[-1], I_y1_arg, A[0], dr_s)[-1]
+            i_y2 = integrate(A[0], A[-1], I_y2_arg, A[0], dr_s)[-1]
+            i_y3 = integrate(A[0], A[-1], I_y3_arg, A[0], dr_s)[-1]
+            zeta = mu / P * (i_t - i_y1)
+            f_0 = mu / (N * P) * (2 * zeta ** 5 * i_z - i_y2 - i_y3 * zeta ** 2)\
+                  + y * zeta ** 3 / N - zeta ** 4 / (np.pi * P)
+
+            y = y - 1 ** -3 / (f_1 - f_0) * (mu / (N * P) * (2 * zeta ** 5 * i_z - i_y2 - i_y3 * zeta ** 2)\
+                  + y * zeta ** 3 / N - zeta ** 4 / (np.pi * P))
             eps = abs(y_old - y)
 
-        i_y1 = integrate(A[0], A[-1], div(mul(mul(mul(mul(X, X), X), mul(G_t, G_t)), mul(G_z, G_z)),
-                                                    mul((y + 2 * I_g), (y + 2 * I_g))), A[0], dr_s)[-1]
+        I_y1_arg = div(mul(mul(mul(X, X), X), mul(mul(G, G), G)),
+                       mul(mul(y + 0.01 + 2 * I_g, y + 0.01 + 2 * I_g), mul(Gam_r, Gam_r)))
+        i_y1 = integrate(A[0], A[-1],  I_y1_arg, A[0], dr_s)[-1]
         zeta = mu / P * (i_t - i_y1)
         a = y / zeta
 
         self.R_s = np.sqrt(a + 2 / zeta * I_g)
         self.R_m = findinverse(self.R_s, A[0], dr_s, dr_m)
 
+        ###
         self.Index_arr_s = []
         self.Index_arr_s.append(0)
         for i in range(0, len(A)-1):
@@ -133,20 +170,15 @@ class GrowTask(wx.Panel):
         print()
 
     def getMaterialRadius(self):
-        return {'r': self.R_s, 'start': self.A[0], 'end': self.A[-1], 'step': self.dr_s,
-                'Radius id': self.Index_arr_s}
+        return {'r': self.R_s, 'New radii': self.R_s[self.Index_arr_s], 'step': self.dr_m}
 
     def getSpatialRadius(self):
-
-        return {'R': self.R_m, 'start': min(self.R_s), 'end': max(self.R_s), 'step': self.dr_m,
-                'Radius id': self.Index_arr_m}
+        return {'R': self.R_m, 'Old radii': self.R_m[self.Index_arr_s], 'step': self.dr_m}
 
     def getMaterialDissplacement(self):
         Diss = self.R_s - [x for x in np.linspace(self.A[0], self.A[-1], len(self.R_s))]
-        return {'Diss': Diss, 'start': self.A[0], 'end': self.A[-1], 'step': self.dr_s,
-                'Radius id': self.Index_arr_s}
+        return {'Diss': Diss, 'Displacement on old radii ': self.R_s[self.Index_arr_s], 'step': self.dr_s}
 
     def getSpatiallDissplacement(self):
         Diss = [x for x in np.linspace(min(self.R_s), max(self.R_s), len(self.R_m))] - self.R_m
-        return {'Diss': Diss, 'start': min(self.R_s), 'end': max(self.R_s), 'step': self.dr_m,
-                'Radius id': self.Index_arr_m}
+        return {'Diss': Diss, 'Displacement on old radii ': self.R_m[self.Index_arr_m], 'step': self.dr_m}
